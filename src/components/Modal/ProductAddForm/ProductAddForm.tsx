3f18f338a8160fbdb6b3868eda6ccaa2c9c43d5f
Leejin-Yang <ejin1996@gmail.com>
2023-09-30 20:08:27 +0900

    feat: 상품 추가 폼 모달 추가

---
+++ src/components/Modal/ProductAddForm/ProductAddForm.tsx
@@ -0,0 +1,126 @@
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  useEffect,
+  useState,
+} from 'react';
+
+import ModalPortal from '../ModalPortal';
+
+import { Category, getProductCategories } from '../../../apis/category';
+import { postProduct } from '../../../apis/product';
+import { CATEGORIES } from '../../../constants';
+
+import {
+  form,
+  formSection,
+  label,
+  submitButton,
+  textarea,
+} from './productAddForm.css';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const INIT_CATEGORIES: Category[] = isDevelopment ? CATEGORIES : [];
+
+interface ProductAddFormProps {
+  onClose: () => void;
+}
+
+const ProductAddForm = ({ onClose }: ProductAddFormProps) => {
+  const [productInfo, setProductInfo] = useState({
+    name: '',
+    price: '',
+    content: '',
+    categoryId: '',
+  });
+  const [categories, setCategories] = useState<Category[]>(INIT_CATEGORIES);
+
+  useEffect(() => {
+    if (isDevelopment) return;
+
+    const fetchCategories = async () => {
+      const data = await getProductCategories();
+      setCategories(data);
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleValueChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+  > = (e) => {
+    const {
+      dataset: { label },
+      value,
+    } = e.target;
+
+    if (!label) return;
+
+    setProductInfo((prev) => ({
+      ...prev,
+      [label]: value,
+    }));
+  };
+
+  const handleProductAdd: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    try {
+      postProduct(productInfo);
+      onClose();
+    } catch {
+      alert('상품 추가 실패');
+    }
+  };
+
+  return (
+    <ModalPortal onClose={onClose}>
+      <form className={form} onSubmit={handleProductAdd}>
+        <label className={label}>
+          <span>상품명</span>
+          <input
+            type='text'
+            className={formSection}
+            onChange={handleValueChange}
+            data-label='name'
+          />
+        </label>
+        <label className={label}>
+          <span>카테고리</span>
+          <select
+            className={formSection}
+            onChange={handleValueChange}
+            data-label='categoryId'
+          >
+            <option value=''>카테고리를 선택하세요</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label className={label}>
+          <span>가격</span>
+          <input
+            type='text'
+            className={formSection}
+            onChange={handleValueChange}
+            data-label='price'
+          />
+        </label>
+        <label className={label}>
+          <span>상품설명</span>
+          <textarea
+            className={textarea}
+            onChange={handleValueChange}
+            data-label='content'
+          />
+        </label>
+        <button className={submitButton}>상품추가</button>
+      </form>
+    </ModalPortal>
+  );
+};
+
+export default ProductAddForm;
---
+++ src/components/Modal/ProductAddForm/productAddForm.css.ts
@@ -0,0 +1,54 @@
+import { style } from '@vanilla-extract/css';
+
+export const form = style({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 30,
+  height: '100%',
+  padding: 30,
+});
+
+export const label = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 10,
+});
+
+export const formSection = style({
+  selectors: {
+    [`${label} &`]: {
+      padding: 10,
+      border: '1px solid #ccc',
+      borderRadius: 4,
+    },
+  },
+});
+
+export const textarea = style([
+  formSection,
+  {
+    selectors: {
+      [`${label} &`]: {
+        resize: 'vertical',
+      },
+    },
+  },
+]);
+
+export const submitButton = style({
+  selectors: {
+    [`${form} &`]: {
+      position: 'absolute',
+      bottom: 30,
+      left: '50%',
+      width: 'calc(100% - 60px)',
+      height: 45,
+      padding: 10,
+      border: '1px solid #ccc',
+      borderRadius: 4,
+      backgroundColor: '#fff',
+      transform: 'translateX(-50%)',
+    },
+  },
+});
