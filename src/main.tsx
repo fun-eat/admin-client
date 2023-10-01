cf6f736c1e2c27eec0b9fdacdc5fbce540cea08b
Leejin-Yang <ejin1996@gmail.com>
2023-10-02 03:37:07 +0900

    feat: 쿼리 클라이언트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import CategoryProvider from './contexts/CategoryContext';
 import App from './App';
 
 import './styles/index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CategoryProvider>
-      <App />
-    </CategoryProvider>
+    <QueryClientProvider client={queryClient}>
+      <CategoryProvider>
+        <App />
+      </CategoryProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
