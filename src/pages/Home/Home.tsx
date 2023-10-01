f941163592a624bcf654ed311588665c93e5f9e5
Leejin-Yang <ejin1996@gmail.com>
2023-10-01 18:17:07 +0900

    feat: Home 페이지 스타일

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,36 @@
 import Layout from '../../components/Layout';
 import { ProductAddForm } from '../../components/Modal';
+import {
+  Colgroup,
+  Table,
+  TableBody,
+  TableHeader,
+} from '../../components/Table';
+
+import { PRODUCT_COLUMNS, PRODUCT_COLUMNS_WIDTH } from '../../constants';
 import { useDisclosure } from '../../hooks';
-import { addButton } from './home.css';
+import { addButton, tableWrapper, title, titleWrapper } from './home.css';
 
 const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Layout>
-      <div>레전드 어드민 커즈하아</div>
-      <button type='button' className={addButton} onClick={onOpen}>
-        상품 추가
-      </button>
+      <div className={titleWrapper}>
+        <h1 className={title}>레전드 어드민 커즈하아</h1>
+        <button type='button' className={addButton} onClick={onOpen}>
+          상품 추가
+        </button>
+      </div>
+      <div></div>
+      <section className={tableWrapper}>
+        <Table>
+          <Colgroup widths={PRODUCT_COLUMNS_WIDTH} />
+          <TableHeader columns={PRODUCT_COLUMNS} />
+          <TableBody />
+        </Table>
+      </section>
+
       {isOpen && <ProductAddForm onClose={onClose} />}
     </Layout>
   );
--- src/pages/Home/home.css.ts
+++ src/pages/Home/home.css.ts
@@ -1,9 +1,29 @@
 import { style } from '@vanilla-extract/css';
 
+export const titleWrapper = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  height: 60,
+  padding: '0 20px',
+});
+
+export const title = style({
+  fontSize: 24,
+  fontWeight: 700,
+});
+
 export const addButton = style({
-  padding: 10,
-  width: 150,
-  height: 45,
+  width: 120,
+  height: 36,
+  lineHeight: '36px',
+  fontSize: 14,
   border: '1px solid #ccc',
   borderRadius: 4,
+});
+
+export const tableWrapper = style({
+  width: '100%',
+  marginTop: 20,
+  padding: '0 20px',
 });
