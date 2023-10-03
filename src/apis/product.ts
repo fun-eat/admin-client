ee4e8bd5c3a97e2e5056787b29821340e8e976b1
Leejin-Yang <ejin1996@gmail.com>
2023-10-03 17:43:51 +0900

    feat: 상품 조회 쿼리 추가

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -8,8 +8,27 @@ export interface ProductResponse {
   categoryResponse: CategoryResponse;
 }
 
-export const getProducts = async (productId: number | null) => {
-  const query = typeof productId === 'number' ? `?productId=${productId}` : '';
+const convertToQueryString = (queryKey: string, value: unknown) =>
+  value ? `${queryKey}=${value}` : '';
+
+export interface ProductRequestQuery {
+  productId: number | null;
+  name?: string;
+  categoryId?: number;
+}
+
+export const getProducts = async ({
+  productId,
+  name,
+  categoryId,
+}: ProductRequestQuery) => {
+  const productIdQuery = convertToQueryString('productId', productId);
+  const nameQuery = convertToQueryString('name', name);
+  const categoryIdQuery = convertToQueryString('categoryId', categoryId);
+  const query = `?${[productIdQuery, nameQuery, categoryIdQuery]
+    .filter((query) => query.length > 0)
+    .join('&')}`;
+
   const response = await fetch(`/api/admin/products${query}`);
   const data: ProductResponse[] = await response.json();
   return data;
