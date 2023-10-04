b885e37ec71a2236794b0fbf3482bee1b90bcf8c
Leejin-Yang <ejin1996@gmail.com>
2023-10-05 02:35:08 +0900

    feat: 리뷰 조회 요청 쿼리 추가

--- src/hooks/queries/useReviewQuery.ts
+++ src/hooks/queries/useReviewQuery.ts
@@ -5,7 +5,7 @@ import { ReviewRequestQuery, getReviews } from '../../apis/review';
 export const useReviewQuery = (queries?: ReviewRequestQuery) => {
   return useQuery({
     queryKey: ['reviews', queries],
-    queryFn: () => getReviews(queries),
+    queryFn: () => getReviews({ reviewId: null, totalElements: null }),
     keepPreviousData: true,
   });
 };
