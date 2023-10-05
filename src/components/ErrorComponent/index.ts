ef192136576335e965bee33f4cf589aba5a9fdec
Leejin-Yang <ejin1996@gmail.com>
2023-10-06 02:02:31 +0900

    fix: 로그인 풀렸을 때 무한 호출 수정

--- src/components/ErrorComponent/index.ts
+++
@@ -1,3 +0,0 @@
-import ErrorComponent from './ErrorComponent';
-
-export default ErrorComponent;
--- src/pages/Layout/AuthLayout.tsx
+++ src/pages/Layout/AuthLayout.tsx
@@ -1,19 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
 
 import { ROUTE } from '../../constants';
 import { useLoginQuery } from '../../hooks/queries';
-import { useQueryClient } from '@tanstack/react-query';
 
 interface AuthLayoutProps {
   children: JSX.Element;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
   const { data: isLoggedIn, isError } = useLoginQuery();
   const queryClient = useQueryClient();
 
-  if (isError) {
-    queryClient.removeQueries({ queryKey: ['login'] });
+  useEffect(() => {
+    if (isError) {
+      queryClient.removeQueries({ queryKey: ['login'] });
+      setShouldRedirect(true);
+    }
+  }, [isError, queryClient]);
+
+  if (shouldRedirect) {
     return <Navigate to={ROUTE.HOME} replace />;
   }
 
