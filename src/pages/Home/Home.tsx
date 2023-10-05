9ac6e528047e19e0f6fb47ce7aa142a0914088ac
Leejin-Yang <ejin1996@gmail.com>
2023-10-05 17:12:42 +0900

    feat: Home 마크업 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
+import Input from '../../components/Input';
+
+import { container, form, submitButton, title } from './home.css';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className={container}>
+      <h1 className={title}>FUNEAT BACK OFFICE</h1>
+      <form className={form}>
+        <Input label='아이디' />
+        <Input label='비밀번호' type='password' />
+        <button className={submitButton}>로그인</button>
+      </form>
+    </main>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/home.css.ts
@@ -0,0 +1,36 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  paddingBottom: '5%',
+});
+
+export const title = style({
+  height: 60,
+  lineHeight: '60px',
+  fontSize: 28,
+  fontWeight: 600,
+  fontStyle: 'italic',
+});
+
+export const form = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+  width: 300,
+  marginTop: 20,
+});
+
+export const submitButton = style({
+  marginTop: 20,
+  height: 45,
+  lineHeight: '45px',
+  fontSize: 16,
+
+  borderRadius: 4,
+  backgroundColor: '#D8EAFF',
+});
