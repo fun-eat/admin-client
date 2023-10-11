8eb566424a3608a3693d6dc2004e922259979ad8
Leejin-Yang <ejin1996@gmail.com>
2023-10-11 20:38:19 +0900

    feat: credential 옵션 수정

--- src/apis/fetchApi.ts
+++ src/apis/fetchApi.ts
@@ -3,12 +3,14 @@ export interface ErrorResponse {
   message: string;
 }
 
-export const fetchApi = async (url: string, options: RequestInit) => {
+type RequestOption = Omit<RequestInit, 'credentials'>;
+
+export const fetchApi = async (url: string, options: RequestOption) => {
   if (!navigator.onLine) {
     throw new Error('네트워크 오프라인이 감지되었습니다');
   }
 
-  const response = await fetch(url, options);
+  const response = await fetch(url, { ...options, credentials: 'include' });
 
   if (!response.ok) {
     throw response;
