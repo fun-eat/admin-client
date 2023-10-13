4cb9395541f8549750b25c9fc13b709abb12dfbe
Leejin-Yang <ejin1996@gmail.com>
2023-10-14 02:44:17 +0900

    feat: 배너 페이지 작성

---
+++ src/pages/Banners/banners.css.ts
@@ -0,0 +1,57 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  maxWidth: 1200,
+  margin: '0 auto',
+});
+
+export const titleWrapper = style([
+  container,
+  {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 60,
+    padding: '0 20px',
+  },
+]);
+
+export const title = style({
+  height: 60,
+  lineHeight: '60px',
+  fontSize: 28,
+  fontWeight: 700,
+});
+
+export const searchSection = style([
+  container,
+  {
+    padding: '0 20px',
+    marginTop: 20,
+  },
+]);
+
+export const addButton = style({
+  width: 120,
+  height: 45,
+  lineHeight: '45px',
+  fontSize: 16,
+  border: '1px solid #ccc',
+  borderRadius: 8,
+});
+
+export const tableWrapper = style([
+  container,
+  {
+    width: '100%',
+    padding: '0 20px',
+    marginTop: 20,
+  },
+]);
+
+export const tableTitle = style({
+  height: 60,
+  lineHeight: '60px',
+  fontSize: 18,
+  fontWeight: 500,
+});
